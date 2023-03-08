e56824b5a6aa88884b7eb0a016654dc0a6fb73b6
dusikowski <97994107+dusikowski@users.noreply.github.com>
2023-03-08 10:25:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,16 +7,20 @@
     <title>dodawanie obecnosci</title>
 </head>
 <body>
+    <form>
     <php?
     $connect=mysqli_connect('localhost','root','','4ti');
     if(connect){
         echo "połczylismy sie z bazą";
-        $zapytanie="SELECT * from przedmiot";
+    
+                $zapytanie="SELECT * from dane";
                 $wynik=mysqli_query($connect,$zapytanie);
                 while($wiersz=mysqli_fetch_array($wynik)){
-                    $przedmiot[]=$wiersz['nazwaPrzedmiot'];
-                    $idprzedmiot[]=$wiersz['id'];
-    }
+                    $imie[]=$wiersz['imie'];
+                    $nazwisko[]=$wiersz['nazwisko'];
+                    $idosoba[]=$wiersz['id'];
+                }
+    
     else
     {
         echo "błąd połczenia";
@@ -27,14 +31,39 @@
         <h1> dodawanie obecnosci</h1>
     </header>
     <main>
+        <table>
+            <thead>
+                <tr>
+                    <th>Lp.</th>
+                    <th>imie i nazwisko</th>
+                    <th>czy jest obecny?</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <?php
+                    for($i=0;$i<count($imie);$i++){
+                        echo "<tr>";
+                        echo "<th>$idosoba[$i]</th>";
+                        echo "<td>$imie[$i]$nazwisko[$i]</td>";
+                        echo "<td><input type='checkbox' name='obecny$idosoba[$i]'id='obecny$idosoba[$i]'></td>";
+                        echo "</tr>";
+                    }
+                    ?>
+                </tr>
+            </tbody>
+        </table>
+        <input type="submit" value="zapisz obecnosc">
+                </form>
     <?php
                 for($i=0;$i<count($imie);$i++){
                     echo "<option value=$idosoba[$i]> $imie[$i] $nazwisko[$i]</option>";
                 }
             ?>
+
     </main>
     <footer>
         dusikowski
     </footer>
 </body>
-</html>
+</html
